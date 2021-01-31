b6b0f6e29a9af68f84ad76f2bca74abee67ea7d0
İbrahim Güngör <igungor@gmail.com>
2021-01-31 15:46:54 +0300

    add temperature/humidity support, print summary on startup

--- susular.ino
+++ susular.ino
@@ -20,21 +20,25 @@
 */
 
 #include <SPI.h>
-#include "LowPower.h"
-#include "RTClib.h"
-#include "SdFat.h"
+#include <LowPower.h>
+#include <RTClib.h>
+#include <SdFat.h>
+#include <DHT.h>
 
 // Pins
 const int ENCODER = 2;
 const int MOTOR_IN1 = 4;
 const int MOTOR_IN2 = 5;
+const int DHT_PIN = 7;
 const int LED = 8;
 const int POWER = 9;
 const int SD_CHIPSELECT = 10;
 
 // Constants
-const int ENCODER_PERIOD = 16;
-const char * fileName = "KAYIT.TXT";
+const int ENCODER_PERIOD = 16; // Number of pulses on encoder wheel
+const char * logfile = "KAYIT.CSV"; // Valve log in TSV format
+const int WATERING_DURATION = 5UL;
+const int WAKEUP_EVERY = 10UL;
 
 // Valve Status
 const byte VALVE_IDLE = 0;
@@ -43,7 +47,14 @@ const byte VALVE_OPEN = 2;
 const byte VALVE_CLOSING = 3;
 const byte VALVE_CLOSED = 4;
 
+// Global variables
+DateTime now;
+float temperature;
+float humidity;
+
 RTC_DS3231 RTC;
+DHT dht(DHT_PIN, DHT11, 3);
+
 
 /*
   const int dayCount = 41;
@@ -92,67 +103,67 @@ RTC_DS3231 RTC;
   };
 */
 const int dayCount = 60;
-DateTime timeToWater[dayCount] = {
-  DateTime(2021, 1, 30, 22, 0, 0),
-  DateTime(2021, 1, 30, 22, 1, 0),
-  DateTime(2021, 1, 30, 22, 2, 0),
-  DateTime(2021, 1, 30, 22, 3, 0),
-  DateTime(2021, 1, 30, 22, 4, 0),
-  DateTime(2021, 1, 30, 22, 5, 0),
-  DateTime(2021, 1, 30, 22, 6, 0),
-  DateTime(2021, 1, 30, 22, 7, 0),
-  DateTime(2021, 1, 30, 22, 8, 0),
-  DateTime(2021, 1, 30, 22, 9, 0),
-  DateTime(2021, 1, 30, 22, 10, 0),
-  DateTime(2021, 1, 30, 22, 11, 0),
-  DateTime(2021, 1, 30, 22, 12, 0),
-  DateTime(2021, 1, 30, 22, 13, 0),
-  DateTime(2021, 1, 30, 22, 14, 0),
-  DateTime(2021, 1, 30, 22, 15, 0),
-  DateTime(2021, 1, 30, 22, 16, 0),
-  DateTime(2021, 1, 30, 22, 17, 0),
-  DateTime(2021, 1, 30, 22, 18, 0),
-  DateTime(2021, 1, 30, 22, 19, 0),
-  DateTime(2021, 1, 30, 22, 20, 0),
-  DateTime(2021, 1, 30, 22, 21, 0),
-  DateTime(2021, 1, 30, 22, 22, 0),
-  DateTime(2021, 1, 30, 22, 23, 0),
-  DateTime(2021, 1, 30, 22, 24, 0),
-  DateTime(2021, 1, 30, 22, 25, 0),
-  DateTime(2021, 1, 30, 22, 26, 0),
-  DateTime(2021, 1, 30, 22, 27, 0),
-  DateTime(2021, 1, 30, 22, 28, 0),
-  DateTime(2021, 1, 30, 22, 29, 0),
-  DateTime(2021, 1, 30, 22, 30, 0),
-  DateTime(2021, 1, 30, 22, 31, 0),
-  DateTime(2021, 1, 30, 22, 32, 0),
-  DateTime(2021, 1, 30, 22, 33, 0),
-  DateTime(2021, 1, 30, 22, 34, 0),
-  DateTime(2021, 1, 30, 22, 35, 0),
-  DateTime(2021, 1, 30, 22, 36, 0),
-  DateTime(2021, 1, 30, 22, 37, 0),
-  DateTime(2021, 1, 30, 22, 38, 0),
-  DateTime(2021, 1, 30, 22, 39, 0),
-  DateTime(2021, 1, 30, 22, 40, 0),
-  DateTime(2021, 1, 30, 22, 41, 0),
-  DateTime(2021, 1, 30, 22, 42, 0),
-  DateTime(2021, 1, 30, 22, 43, 0),
-  DateTime(2021, 1, 30, 22, 44, 0),
-  DateTime(2021, 1, 30, 22, 45, 0),
-  DateTime(2021, 1, 30, 22, 46, 0),
-  DateTime(2021, 1, 30, 22, 47, 0),
-  DateTime(2021, 1, 30, 22, 48, 0),
-  DateTime(2021, 1, 30, 22, 49, 0),
-  DateTime(2021, 1, 30, 22, 50, 0),
-  DateTime(2021, 1, 30, 22, 51, 0),
-  DateTime(2021, 1, 30, 22, 52, 0),
-  DateTime(2021, 1, 30, 22, 53, 0),
-  DateTime(2021, 1, 30, 22, 54, 0),
-  DateTime(2021, 1, 30, 22, 55, 0),
-  DateTime(2021, 1, 30, 22, 56, 0),
-  DateTime(2021, 1, 30, 22, 57, 0),
-  DateTime(2021, 1, 30, 22, 58, 0),
-  DateTime(2021, 1, 30, 22, 59, 0),
+DateTime schedule[dayCount] = {
+  DateTime(2021, 1, 31, 15, 0, 0),
+  DateTime(2021, 1, 31, 15, 1, 0),
+  DateTime(2021, 1, 31, 15, 2, 0),
+  DateTime(2021, 1, 31, 15, 3, 0),
+  DateTime(2021, 1, 31, 15, 4, 0),
+  DateTime(2021, 1, 31, 15, 5, 0),
+  DateTime(2021, 1, 31, 15, 6, 0),
+  DateTime(2021, 1, 31, 15, 7, 0),
+  DateTime(2021, 1, 31, 15, 8, 0),
+  DateTime(2021, 1, 31, 15, 9, 0),
+  DateTime(2021, 1, 31, 15, 10, 0),
+  DateTime(2021, 1, 31, 15, 11, 0),
+  DateTime(2021, 1, 31, 15, 12, 0),
+  DateTime(2021, 1, 31, 15, 13, 0),
+  DateTime(2021, 1, 31, 15, 14, 0),
+  DateTime(2021, 1, 31, 15, 15, 0),
+  DateTime(2021, 1, 31, 15, 16, 0),
+  DateTime(2021, 1, 31, 15, 17, 0),
+  DateTime(2021, 1, 31, 15, 18, 0),
+  DateTime(2021, 1, 31, 15, 19, 0),
+  DateTime(2021, 1, 31, 15, 20, 0),
+  DateTime(2021, 1, 31, 15, 21, 0),
+  DateTime(2021, 1, 31, 15, 22, 0),
+  DateTime(2021, 1, 31, 15, 23, 0),
+  DateTime(2021, 1, 31, 15, 24, 0),
+  DateTime(2021, 1, 31, 15, 25, 0),
+  DateTime(2021, 1, 31, 15, 26, 0),
+  DateTime(2021, 1, 31, 15, 27, 0),
+  DateTime(2021, 1, 31, 15, 28, 0),
+  DateTime(2021, 1, 31, 15, 29, 0),
+  DateTime(2021, 1, 31, 15, 30, 0),
+  DateTime(2021, 1, 31, 15, 31, 0),
+  DateTime(2021, 1, 31, 15, 32, 0),
+  DateTime(2021, 1, 31, 15, 33, 0),
+  DateTime(2021, 1, 31, 15, 34, 0),
+  DateTime(2021, 1, 31, 15, 35, 0),
+  DateTime(2021, 1, 31, 15, 36, 0),
+  DateTime(2021, 1, 31, 15, 37, 0),
+  DateTime(2021, 1, 31, 15, 38, 0),
+  DateTime(2021, 1, 31, 15, 39, 0),
+  DateTime(2021, 1, 31, 15, 40, 0),
+  DateTime(2021, 1, 31, 15, 41, 0),
+  DateTime(2021, 1, 31, 15, 42, 0),
+  DateTime(2021, 1, 31, 15, 43, 0),
+  DateTime(2021, 1, 31, 15, 44, 0),
+  DateTime(2021, 1, 31, 15, 45, 0),
+  DateTime(2021, 1, 31, 15, 46, 0),
+  DateTime(2021, 1, 31, 15, 47, 0),
+  DateTime(2021, 1, 31, 15, 48, 0),
+  DateTime(2021, 1, 31, 15, 49, 0),
+  DateTime(2021, 1, 31, 15, 50, 0),
+  DateTime(2021, 1, 31, 15, 51, 0),
+  DateTime(2021, 1, 31, 15, 52, 0),
+  DateTime(2021, 1, 31, 15, 53, 0),
+  DateTime(2021, 1, 31, 15, 54, 0),
+  DateTime(2021, 1, 31, 15, 55, 0),
+  DateTime(2021, 1, 31, 15, 56, 0),
+  DateTime(2021, 1, 31, 15, 57, 0),
+  DateTime(2021, 1, 31, 15, 58, 0),
+  DateTime(2021, 1, 31, 15, 59, 0),
 };
 
 // 1 hour buffer for date comparison
@@ -178,48 +189,77 @@ void setup() {
     disableRTC();
     powerOffPeripherals();
   }
+
+  printSummary();
 }
 
 void loop() {
-  Serial.println(F("here we go! checking the date..."));
   if (!isTimeToWater()) {
-    Serial.println(F("not my time, sleeping an hour"));
-    Serial.flush(); // wait for the tranmission to end before going sleep
-    record(VALVE_IDLE, 0, 0);
+    Serial.println(F("sulama vakti degil. uykuya geciyorum..."));
+    Serial.flush();
+    record(VALVE_IDLE);
 
     // sleep for an hour until next check
-    // sleepFor(1 * 60 * 60);
-    sleepFor(1);
+    sleepFor(WAKEUP_EVERY);
     return;
   }
 
-  Serial.println(F("watering time! opening the valve..."));
+  Serial.println(F("sulama vakti! vanayi aciyorum..."));
 
   // open the valve
-  record(VALVE_OPENING, 0, 0);
+  record(VALVE_OPENING);
   turnMotorLeft(1.5);
   stopMotor();
-  record(VALVE_OPEN, 0, 0);
+  record(VALVE_OPEN);
 
   // keep the valve open and let the plants get some water
-  Serial.println(F("valve open. sleeping 4 hours..."));
+  Serial.println(F("vana acildi. sulama bitene kadar uyuyorum..."));
   Serial.flush();
-  // powerDownFor(4 * 60 * 60);
-  sleepFor(2);
+  sleepFor(WATERING_DURATION);
 
   // close the valve
-  Serial.println(F("slept 4 hours. closing the valve..."));
-  record(VALVE_CLOSING, 0, 0);
+  Serial.println(F("sulama bitti. vanayi kapiyorum..."));
+  record(VALVE_CLOSING);
+
   turnMotorRight(1.5);
   stopMotor();
-  record(VALVE_CLOSED, 0, 0);
 
-  Serial.println(F("watering done! sleeping half a day..."));
+  record(VALVE_CLOSED);
+  Serial.println(F("vana kapandi. uyku vakti..."));
   Serial.flush(); // wait for the tranmission to end before going sleep
 
   // save battery
-  // powerDownFor(12 * 60 * 60UL);
-  sleepFor(2);
+  sleepFor(WAKEUP_EVERY);
+}
+
+void printSummary() {
+  powerOnPeripherals();
+  getTime();
+  getTemperature();
+
+  Serial.println(F("### Vana1 ###"));
+  printTime();
+  printTemperature();
+  Serial.println();
+
+  int found = 0;
+  Serial.println(F("### Sulama takvimi ###"));
+  for (int i = 0; i < dayCount; i++) {
+    DateTime d = schedule[i];
+    String ds = dateTimeToString(d);
+    if (now < d) {
+      if (!found) {
+        Serial.print(ds);
+        Serial.println(F("  <--- Siradaki sulama"));
+        found = 1;
+      } else {
+        Serial.println(ds);
+      }
+    } else {
+      Serial.println(ds);
+    }
+  }
+  Serial.println(F("=============="));
 }
 
 // shutdown all units for given seconds.
@@ -240,6 +280,7 @@ void sleepFor(unsigned long seconds) {
 void powerOnPeripherals() {
   pinMode(POWER, OUTPUT);
   digitalWrite(POWER, LOW); // p-channel mosfet. low -> active.
+  delay(100); // wait a few ms for components to initialize
 }
 
 void powerOffPeripherals() {
@@ -271,41 +312,15 @@ void disableRTC () {
   digitalWrite (A5, LOW);
 }
 
-void flashLED (const byte times) {
-  for (int i = 0; i < times; i++)
-  {
-    digitalWrite (LED, HIGH);
-    delay (20);
-    digitalWrite (LED, LOW);
-    delay (150);
-  }
+void getTime() {
+  enableRTC();
+  now = RTC.now();
+  disableRTC();
 }
 
-bool isTimeToWater() {
-  powerOnPeripherals();
-  delay(10); // wait a few ms for rtc
-  enableRTC();
-
-  flashLED(3);
-  DateTime now = RTC.now();
-  disableRTC();
-  powerOffPeripherals();
-
-  printTime(now);
-
-  Serial.println(F("iterating over watering days..."));
-  for (int i = 0; i < dayCount; i++ ) {
-    DateTime timeStart = timeToWater[i];
-    DateTime timeEnd = timeStart + dateBuffer;
-
-    if ((now >= timeStart) && (now <= timeEnd)) {
-      Serial.print(F("watering time found: "));
-      Serial.println(dateTimeToString(timeStart));
-      return true;
-    }
-  }
-
-  return false;
+void printTime() {
+  Serial.print(F("saat: "));
+  Serial.println(dateTimeToString(now));
 }
 
 String dateTimeToString(DateTime &dt) {
@@ -318,25 +333,68 @@ String dateTimeToString(DateTime &dt) {
   return String(buffer);
 }
 
-void printTime(DateTime now) {
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+
+void enableDHT() {
+  dht.begin();
 }
 
-void record(unsigned int valveStatus,
-            int temperature,
-            float humidity) {
+void disableDHT() {
+  digitalWrite(DHT_PIN, LOW);
+}
 
+void getTemperature() {
+  enableDHT();
+  delay(2000);
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println(F("dht: failed to read from sensor!"));
+    disableDHT();
+    return;
+  }
+  disableDHT();
+}
+
+void printTemperature() {
+  Serial.print(F("sicaklik: "));
+  Serial.print(temperature);
+  Serial.print(F(" *C, nem: %"));
+  Serial.println(humidity);
+}
+
+void flashLED (const byte times) {
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite (LED, HIGH);
+    delay (20);
+    digitalWrite (LED, LOW);
+    delay (150);
+  }
+}
+
+bool isTimeToWater() {
+  powerOnPeripherals();
+  getTime();
+  powerOffPeripherals();
+
+  Serial.println(F("sulama takvimine bakiyorum..."));
+  for (int i = 0; i < dayCount; i++ ) {
+    DateTime timeStart = schedule[i];
+    DateTime timeEnd = timeStart + dateBuffer;
+
+    if ((now >= timeStart) && (now <= timeEnd)) {
+      Serial.print(F("sulama vakti gelmis: "));
+      Serial.println(dateTimeToString(timeStart));
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void record(unsigned int valveStatus) {
   powerOnPeripherals();
 
   SdFat sd;
@@ -352,14 +410,13 @@ void record(unsigned int valveStatus,
   SdFile file;
   SPI.begin();
 
-  if (!file.open(fileName, O_CREAT | O_WRITE | O_APPEND)) {
+  if (!file.open(logfile, O_CREAT | O_WRITE | O_APPEND)) {
     Serial.println(F("sd: failed to open file"));
     Serial.flush();
     return;
   }
-  enableRTC();
-  DateTime now = RTC.now();
-  disableRTC();
+  getTime();
+  getTemperature();
 
   /*
      Datetime | ValveStatus | Temperature | Humidity
@@ -376,6 +433,10 @@ void record(unsigned int valveStatus,
   file.print(buf);
   file.print("\t");
   file.print(valveStatus);
+  file.print("\t");
+  file.print(temperature);
+  file.print("\t");
+  file.print(humidity);
   file.println();
 
   file.sync();
